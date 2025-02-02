33987a02d692858784442b10bcfb083bf5cac45a
Md. Istiaque Khalique <75970815+Istiaque-here@users.noreply.github.com>
2025-02-02 16:32:18 +0000

    last commit from codespace

--- test.py
+++ test.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+blogs = [
+    {
+        'author':'mark',
+        'title':'hello world',
+        'content':'hi this mark from nowhere',
+        'date_posted':'january 31,2025'
+    },
+    {
+        'author':'twain',
+        'title':'hello mars',
+        'content':'hi this twain from mars',
+        'date_posted':'february 1,2025'
+    },
+]
 @app.route('/')
 def home():
-    return "Hello, Flask in Codespaces! Myself Istiaque here."
+    # return "Hello, Flask in Codespaces! Myself Istiaque here."
+    return render_template('home.html',posts=blogs)
+
+@app.route('/about')
+def about():
+    return render_template('about.html',title='Istiaque')
 
 app.run(debug=True)
